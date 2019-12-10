b10da289d06dfba519017f5013db6644741c9fbd
JlFerrete <joseluisferrete@gmail.com>
2019-12-10 19:51:44 +0100

    Added watch to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,14 @@ const autoprefixer = require('gulp-autoprefixer');
 function css() {
     return gulp
         .src('scss/app.scss')
-        .pipe(autoprefixer() )
+        .pipe(autoprefixer())
         .pipe(sass({ outputStyle: "expanded" }))
         .pipe(gulp.dest("css"))
 }
+
 function watchFiles() {
     gulp.watch('scss/*.scss', css);
-    gulp.watch('index.html')
+    gulp.watch('index.html');
 }
 
 // tasks
